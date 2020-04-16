f9b690c3e1b8d92e7eeccf677b55ce756c92bcc6
andy <andy@pilotandy.com>
2020-04-16 11:33:22 -0500

    Fix aircraft model

--- aircraft/models.py
+++ aircraft/models.py
@@ -9,7 +9,7 @@ class Aircraft(models.Model):
     name = models.CharField(max_length=10)
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     public = models.BooleanField(default=False)
-    model = JSONField()
+    model = JSONField(default=dict, blank=True)
 
     def __str__(self):
         return self.owner.firstname + ' ' + self.owner.lastname + ': ' + self.name
